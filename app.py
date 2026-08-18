ed0248227573e0f11091d729136464ed1b69ab45
musto <153368670+wgysx@users.noreply.github.com>
2026-08-18 11:15:52 +0000

    gitadd

--- app.py
+++ app.py
@@ -9,3 +9,11 @@ def index():
 @app.route("/boletim")
 def boletim():
     return render_template('boletim.html')
+
+@app.route("/sobre")
+def sobre():
+    return render_template('sobre.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
